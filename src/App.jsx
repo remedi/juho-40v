667ab2f9feb5cc831b4075b9f32dbcd13c5f89c6
Markus Palonen <markus.palonen@gmail.com>
2025-08-30 09:47:13 +0300

    Add content

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import photo from '/photo.jpg'
 import './App.css'
 
 function App() {
@@ -8,26 +7,21 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h3>Juho 40v</h3>
+      <h1>EräRetki 2026</h1>
+      <p>40 vuotta elämää huipentuu kärsimykseen erämaan armoilla.</p>
+      <div className="flex">
+        <img src={photo} className="logo" alt="JUHO" />
       </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => (window.location="https://forms.gle/39XGBmzV9nnXfzwv9")}>
+          Valitse vaihtoehdot
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="read-the-docs">
+        Lisää infoa tulossa!
+      </div>
     </>
   )
 }
